8a32bd40d3b69d8487d20f44602ddf35c8a84372
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-04-24 05:52:18 +0900

    [WIP] Add entry_points for roslint catkin_tools verb

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup(**generate_distutils_setup(
     packages=['roslint'],
     package_dir={
-      '': 'src'
-      }
+        '': 'src'
+    },
+    entry_points={
+        'catkin_tools.commands.catkin.verbs': [
+            'roslint = roslint.main:description',
+        ],
+    },
 ))
